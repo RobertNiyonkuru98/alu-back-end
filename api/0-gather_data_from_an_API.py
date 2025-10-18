8390d07c71112c38850bad465cf277028f5fc4ab
Robert <r.niyonkuru@alustudent.com>
2025-10-18 21:14:44 +0200

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
     total_number_of_tasks = len(todos_info)
 
     print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_task))
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
     [print("\t" + task["title"]) for task in task_completed]
